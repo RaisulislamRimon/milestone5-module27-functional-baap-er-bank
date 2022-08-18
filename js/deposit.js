204535a23747f066a0c141a998cc7aca1e12ba52
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-18 13:02:07 +0600

    finished module 27-7 (advanced) get and set element value using a function

--- js/deposit.js
+++ js/deposit.js
@@ -1,20 +1,11 @@
-function getInputFieldValueById(inputFieldId) {
-  const inputField = document.getElementById(inputFieldId);
-  const inputFieldValueString = inputField.value;
-  const inputFieldValue = parseFloat(inputFieldValueString);
-  inputField.value = "";
-  return inputFieldValue;
-}
-
-function getElementValueById(elementId) {
-  const element = document.getElementById(elementId);
-  const elementValueString = element.innerText;
-  const elementValue = parseFloat(elementValueString);
-  return elementValue;
-}
-
 document.getElementById("btn-deposit").addEventListener("click", function () {
   const newDepositAmount = getInputFieldValueById("deposit-field");
+  const previousDepositTotal = getTextElementValueById("deposit-total");
+  const newDepositTotal = previousDepositTotal + newDepositAmount;
+  setTextElementValueById("deposit-total", newDepositTotal);
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  setTextElementValueById("balance-total", newBalanceTotal);
 });
 
 /* 
