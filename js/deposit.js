b282a9ce6aa3e05f88f033c1490a3dcab35c3396
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-18 12:37:27 +0600

    finished module 27-6 (advanced) Share one function between two input field

--- js/deposit.js
+++ js/deposit.js
@@ -1,11 +1,20 @@
-function getInputFieldValueById(inputId) {
-  const inputField = document.getElementById(inputId);
+function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
-  return inputFieldValueString;
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = "";
+  return inputFieldValue;
+}
+
+function getElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueString = element.innerText;
+  const elementValue = parseFloat(elementValueString);
+  return elementValue;
 }
 
 document.getElementById("btn-deposit").addEventListener("click", function () {
-  console.log("deposit btn clicked");
+  const newDepositAmount = getInputFieldValueById("deposit-field");
 });
 
 /* 
