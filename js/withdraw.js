00c42c6824153733ae006ba1f9e1fc5e1a3c5000
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-18 13:27:13 +0600

    finished module 27-8 (advanced) Shared Balance update function

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,3 +1,13 @@
+document.getElementById("btn-withdraw").addEventListener("click", function () {
+  const newWithdrawAmount = getInputFieldValueById("withdraw-field");
+  const previousWithdrawTotal = getTextElementValueById("withdraw-total");
+  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  setTextElementValueById("withdraw-total", newWithdrawTotal);
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+  setTextElementValueById("balance-total", newBalanceTotal);
+});
+
 /* 
 1. add event handler with the withdraw button
 
